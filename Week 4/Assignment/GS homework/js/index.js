8baac5aa7f171c0f2f6a06a54a9dcd23ef323c7c
Grant <grant.sadowski@gmail.com>
2018-01-02 23:08:16 -0500

    HW4 upload
    Week 4 assignment upload

--- Week 4/Assignment/GS homework/js/index.js
+++ Week 4/Assignment/GS homework/js/index.js
@@ -1,23 +1,40 @@
 $(document).ready(function() {
 	//alert("hello world");
 
-	$('.readmore').click(showMoreText);
-	$('.readless').click(showLessText);
+	$('.readmore-1').click(showMoreText);
+	$('.readmore-2').click(showMoreText2);
+	$('.readless-1').click(showLessText);
+	$('.readless-2').click(showLessText2);
 	$('.learnmore').click(revealSomething);
-	$('header a').hover(hoverEffect);
+	$('#bottom-cta a').click(dialogBox);
+	
 
 	function showMoreText() {
 		event.preventDefault();
-		$('.readmore').hide();
+		$('.readmore-1').hide();
 		$('#show-this-on-click').slideDown();
-		$('.readless').show();
+		$('.readless-1').show();
+	}
+	
+	function showMoreText2() {
+	  event.preventDefault();
+	  $('.readmore-2').hide();
+	  $('#show-this-on-click-2').slideDown();
+	  $('.readless-2').show();
 	}
 
 	function showLessText() {
 		event.preventDefault();
-		$('.readmore').show();
+		$('.readmore-1').show();
 		$('#show-this-on-click').slideUp();
-		$('.readless').hide();		
+		$('.readless-1').hide();
+	}
+	
+	function showLessText2 () {
+	  event.preventDefault();
+	  $('.readmore-2').show();
+	  $('#show-this-on-click-2').slideUp();
+	  $('.readless-2').hide();
 	}
 
 	function revealSomething() {
@@ -26,8 +43,20 @@ $(document).ready(function() {
 		$('#sidebar #learnmoretext').slideDown();
 	}
 
-	function hoverEffect() {
-		$('header a').css('');
+	function dialogBox() {
+		//alert('Hello!!!');
+	  event.preventDefault();
+	  
+	  $('#dialog').dialog({
+	    autoOpen: false,
+	    resizable: false,
+	    draggable: false,
+	    width: 500,
+	    show: 'slideDown'
+	    
+	  });
+	  
+		$('#dialog').dialog('open');
 	}
 
 
